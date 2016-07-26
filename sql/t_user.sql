30d46b8f61cebaffaec81f2bbdd33653091064c3
xiaokai365 <snail_hl_365@163.com>
2016-07-26 11:05:18 +0800

    传统项目初始化

---
+++ sql/t_user.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50624
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50624
+File Encoding         : 65001
+
+Date: 2016-07-26 10:59:40
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `t_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1', '123', 'zhangsan');
---
+++ src/main/java/com/snail/dubbo/service/impl/UserserviceImpl.java
@@ -0,0 +1,22 @@
+package com.snail.dubbo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.snail.dubbo.mapper.UserMapper;
+import com.snail.dubbo.pojo.User;
+import com.snail.dubbo.service.Userservice;
+
+@Service
+public class UserserviceImpl implements Userservice{
+
+	@Autowired
+	private UserMapper userMapper; 
+	
+	@Override
+	public int save(User user) {
+		
+		return userMapper.save(user);
+	}
+
+}
